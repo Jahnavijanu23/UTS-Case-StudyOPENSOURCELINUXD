d2d4cbd556b35274d66ad16d9e07c6ff04322d50
Jahnavijanu23 <jahnavi.24mip10157@vitbhopal.ac.in>
2026-03-31 22:18:44 +0530

    Add files via upload

---
+++ script1 (1).sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+# Script 1: System Identity Report
+# Author: Jahnavi (24MIP10157)
+# Course: Open Source Software
+
+# --- Variables ---
+STUDENT_NAME="Jahnavi"
+REG_NO="24MIP10157"
+SOFTWARE_CHOICE="Git"
+
+# --- System Info ---
+KERNEL=$(uname -r)
+USER_NAME=$(whoami)
+HOME_DIR=$HOME
+UPTIME=$(uptime -p)
+DISTRO=$(cat /etc/os-release | grep "^PRETTY_NAME" | cut -d '=' -f 2 | tr -d '"')
+CURRENT_DATE=$(date)
+
+# --- Display ---
+echo "========================================="
+echo "       Open Source Audit Welcome         "
+echo "========================================="
+echo "Student Name    : $STUDENT_NAME"
+echo "Registration No : $REG_NO"
+echo "Chosen Software : $SOFTWARE_CHOICE"
+echo "-----------------------------------------"
+echo "OS Distribution : $DISTRO"
+echo "Kernel Version  : $KERNEL"
+echo "Current User    : $USER_NAME"
+echo "Home Directory  : $HOME_DIR"
+echo "System Uptime   : $UPTIME"
+echo "Current Date    : $CURRENT_DATE"
+echo "-----------------------------------------"
+echo "License Note: This Ubuntu OS is largely governed by the GPL license!"
+echo "========================================="
